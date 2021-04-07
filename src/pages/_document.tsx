c828fd1b8e2032203da6484cb71ab13f528fb1c2
Georgina Rojas <ginarojas29@gmail.com>
2021-04-07 20:00:17 -0300

    .

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
             rel="stylesheet"
-          ></link>
+          />
         </Head>
         <body>
           <Main />
