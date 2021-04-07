872add5a0d7afbf760530fbf75b51e45af044399
Georgina Rojas <ginarojas29@gmail.com>
2021-04-07 15:25:11 -0300

    Sign In page created.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,66 @@
+import {
+  Flex,
+  Input,
+  Button,
+  Stack,
+  FormLabel,
+  FormControl,
+} from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <h1>Hello</h1>
-    )
+    <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
+      {/* "8"  is a measure from chakra (8*4=32px)
+       but {8} is 8px.
+      https://chakra-ui.com/docs/theming/theme
+      */}
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        background="gray.800"
+        padding="8"
+        borderRadius={8}
+        flexDirection="column"
+      >
+        {/* Stack: is used to style stacked elements */}
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              name="email"
+              type="email"
+              id="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bg: "gray.900",
+              }}
+              size="lg"
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+            <Input
+              name="password"
+              type="password"
+              id="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bg: "gray.900",
+              }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+        <Button type="submit" marginTop="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
