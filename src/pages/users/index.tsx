b756c8239a24472b4ad5585e910f0c68851149fa
Georgina Rojas <ginarojas29@gmail.com>
2021-04-07 21:52:21 -0300

    Building the Users page.

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,93 @@
+import React from "react";
+import { Header } from "../../components/Header";
+import { SiderBar } from "../../components/SideBar";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+      <Flex
+        width="100%"
+        marginY="6"
+        maxWidth={1480}
+        marginX="auto"
+        paddingX="20"
+      >
+        <SiderBar />
+
+        <Box flex="1" borderRadius={8} background="gray.800" padding="8">
+          <Flex marginBottom="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20"/>}
+            >
+              Criar novo
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th paddingX="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de cadastro</Th>
+                <Th width="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td paddingX="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Georgina Rojas</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      ginarojas29@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>07 de abirl, 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="pink"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
